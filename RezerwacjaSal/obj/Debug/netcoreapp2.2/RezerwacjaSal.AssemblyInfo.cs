d73c0b61e5c0383b07814189e76bbd17dbbeea16
Krzysztof <krzysztof.cislo5@gmail.com>
2018-12-10 14:29:54 +0100

    Aktualizacja Boostrap 3 do 4

--- RezerwacjaSal/obj/Debug/netcoreapp2.2/RezerwacjaSal.AssemblyInfo.cs
+++ RezerwacjaSal/obj/Debug/netcoreapp2.2/RezerwacjaSal.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RezerwacjaSal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Kontynuacja projektu IO jako AI")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.9.9.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.9.9")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RezerwacjaSal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RezerwacjaSal")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.9.9.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
 
